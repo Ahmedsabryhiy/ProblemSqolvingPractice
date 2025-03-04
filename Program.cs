77fbf4915000d3e9d739adbb40c2c1d6596289eb
Ahmed sabri <AhmedSabry@gmail.com>
2025-03-04 15:01:23 +0200

    add find second largest number in array

--- Program.cs
+++ Program.cs
@@ -6,32 +6,35 @@
         {
             string text = "C# is fun . learing C# is enjoyable . c# is everwhere.";
             string pattern = "C#";
-          
+
             int[] array1 = { 1, 3, 5, 7, 10 };
             int[] array2 = { 2, 4, 6, 8 };
             int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
             int[] arr2 = { 1, 2, 3, 1, 2, 3, 1, 2, 3 };
 
             Console.WriteLine("Welcome to Problem Solving Practice");
-          
-            string choice="cont";
-            while ( choice != "exit") 
-            {  
-            
-               
-               
+
+            string choice = "cont";
+            while (choice != "exit")
+            {
+
+
+
                 Console.WriteLine("1- Count Occurrences");
                 Console.WriteLine("2- Reverse Order Of Words");
                 Console.WriteLine("3- First Non Repeating");
-               
-         
+
+
                 Console.WriteLine("4- Merge Two Arrays");
-                Console .WriteLine("5- Maximum Subarray Sum ");
+                Console.WriteLine("5- Maximum Subarray Sum ");
                 Console.WriteLine("6- Palindrome");
                 Console.WriteLine("7- Remove Duplicates from an Array");
                 Console.WriteLine("8- Factorial of a number");
                 Console.WriteLine("9- FizzBuzz Problem");
-                
+                Console.WriteLine("10- Remove Duplicates from an Array");
+                Console.WriteLine("11- Sum of Digits");
+                Console.WriteLine("12- Find Second Largest Number");
+
                 Console.WriteLine("0- Exit");
 
                 Console.WriteLine("Enter the number of the problem you want to solve");
@@ -41,25 +44,25 @@
                 switch (number)
                 {
                     case 1:
-                        CountOccurrences( text ,pattern);
+                        CountOccurrences(text, pattern);
                         break;
                     case 2:
-                        ReverseOrderOfWords( text);
+                        ReverseOrderOfWords(text);
                         break;
                     case 3:
                         FirstNonRepeating("stress");
                         break;
-                
+
                     case 4:
                         MergeTowArraies(array1, array2);
                         break;
                     case 5:
-                        int maxSum =MaximumSubarraySum(arr);
+                        int maxSum = MaximumSubarraySum(arr);
                         Console.WriteLine($"The maximum subarray sum is: {maxSum}");
                         break;
                     case 6:
                         string word = "A man, a plan, a canal: Panama";
-                        Palindrome(word );
+                        Palindrome(word);
                         break;
                     case 7:
                         RemoveDuplicates(arr2);
@@ -72,9 +75,17 @@
                     case 9:
                         FizzBuzz();
                         break;
-                        case 10:
-                           int[] result1 =  RemoveDuplicate(arr2);
-                            Console.WriteLine($"The unique elements are: {string.Join(", ", result1)}");
+                    case 10:
+                        int[] result1 = RemoveDuplicate(arr2);
+                        Console.WriteLine($"The unique elements are: {string.Join(", ", result1)}");
+                        break;
+                        case 11:
+                        SumOfDigitsWithoutDivision(14789);
+                        break;
+                        case 12:
+                        int[] ints = { 1, 5, 3, 4, 5, 6, 7 };
+                           Console.WriteLine ( FindSecondLargestNumber  (ints));
+
                             break;
                     case 0:
                         return;
@@ -86,16 +97,16 @@
                 choice = Console.ReadLine();
             }
 
-            
-           Console.ReadLine();
+
+            Console.ReadLine();
 
 
-        
+
         }
 
         private static void Palindrome(string word)
         {
-          string reversedWord = ReverseOrderOfWords(word);
+            string reversedWord = ReverseOrderOfWords(word);
             if (word == reversedWord)
             {
                 Console.WriteLine("The word is a palindrome");
@@ -107,7 +118,7 @@
 
 
 
-           
+
 
 
         }
@@ -127,14 +138,14 @@
         }
         static string ReverseOrderOfWords(string text)
         {
-           
+
             string[] words = text.Split(' ');
             string reversedText = "";
             for (int i = words.Length - 1; i >= 0; i--)
             {
                 reversedText += words[i] + " ";
             }
-            return reversedText ;
+            return reversedText;
         }
         static char FirstNonRepeating(string word)
         {
@@ -158,8 +169,8 @@
         {
 
 
-             int[] mergedArray = new int[a.Length + b.Length];
-         int index = 0;
+            int[] mergedArray = new int[a.Length + b.Length];
+            int index = 0;
             for (int i = 0; i < a.Length; i++)
             {
                 mergedArray[index] = a[i];
@@ -177,14 +188,14 @@
 
 
         }
-        static int MaximumSubarraySum (int[] a)
+        static int MaximumSubarraySum(int[] a)
         {
-            int  currentMax=a[0];
+            int currentMax = a[0];
             int gloalMax = a[1];
-            for(int i = 0; i<a.Length; i++)
+            for (int i = 0; i < a.Length; i++)
             {
-                 currentMax=Math.Max(a[i], currentMax + a[i]);
-                gloalMax =Math .Max(gloalMax, currentMax);
+                currentMax = Math.Max(a[i], currentMax + a[i]);
+                gloalMax = Math.Max(gloalMax, currentMax);
 
 
             }
@@ -209,7 +220,7 @@
             int sum = 1;
             if (number == 0 || number == 1)
                 return 1;
-           else
+            else
             {
                 for (int i = 1; i <= number; i++)
                 {
@@ -217,18 +228,18 @@
                 }
             }
             return sum;
-              
+
         }
         static void FizzBuzz()
         {
-            for(int i = 1; i <=100;i++)
+            for (int i = 1; i <= 100; i++)
             {
                 if (i % 3 == 0)
                     Console.WriteLine("Fizz");
                 else if (i % 5 == 0)
                     Console.WriteLine("Buzz");
-               
-               else if ( i % 3 == 0 && i % 5 == 0)
+
+                else if (i % 3 == 0 && i % 5 == 0)
                     Console.WriteLine("FizzBuzz");
                 else Console.WriteLine(i);
             }
@@ -240,14 +251,14 @@
             int uniqueCount = 0;
             for (int i = 0; i < a.Length; i++)
             {
-                for (int j=0;j<uniqueCount;j++)
+                for (int j = 0; j < uniqueCount; j++)
                 {
                     if (result[j] == a[i])
                     {
                         alreadyExists = true;
                         break;
                     }
-                   
+
                 }
                 if (!alreadyExists)
                 {
@@ -255,12 +266,101 @@
                     uniqueCount++;
                 }
                 alreadyExists = false;
+
+            }
+            return result;
+
+        }
+    
+
+
+        // Method to compute the sum of digits without using '/' or '%' operators.
+        public static int SumOfDigitsWithoutDivision(int number)
+        {
+            // Handle negative numbers manually.
+            if (number < 0)
+            {
+                number = -number;
+            }
+
+            int sum = 0;
+
+            // Process until number is reduced to 0.
+            while (number > 0)
+            {
+                int quotient = 0;
+                int remainder = number; // Start with the full number.
+
+                // Repeatedly subtract 10 from 'remainder' until it is less than 10.
+                // The number of times we subtract 10 gives us the quotient.
+                while (remainder >= 10)
+                {
+                    remainder -= 10;  // Subtract 10.
+                    quotient++;       // Count how many times we subtracted.
+                }
+
+                // 'remainder' now represents the last digit (like number % 10).
+                sum += remainder;
+
+                // 'quotient' simulates number / 10. Update number.
+                number = quotient;
+            }
+
+            return sum;
+        }
+
+        static int FindSecondLargestNumber(int[] a)
+        {
+            //if (a.Length < 2)
+            //    throw new ArgumentException("Array must contain at least two elements.");
+
+            //int largest = int.MinValue;
+            //int secondLargest = int.MinValue;
+
+            //foreach (int num in a)
+            //{
+            //    if (num > largest)
+            //    {
+            //        secondLargest = largest;
+            //        largest = num;
+            //    }
+            //    else if (num > secondLargest && num < largest)
+            //    {
+            //        secondLargest = num;
+            //    }
+            //}
+
+            //if (secondLargest == int.MinValue)
+            //    throw new InvalidOperationException("No second largest element found.");
+
+            //return secondLargest;
+            if (a.Length < 2)
+                throw new ArgumentException("Array must contain at least two elements.");
+
+            int largest = int.MinValue;
+            int secondLargest = int.MinValue;
+            for (int i = 0; i < a.Length; i++) 
+            { 
+
+               if (a[i] > largest)
+                {
+                    secondLargest = largest;
+                    largest = a[i];
+                }
+                else if (a[i] > secondLargest && a[i] < largest)
+                {
+                    secondLargest = a[i];
+                }
                
             }
-           return   result;
-                
+            return secondLargest;
+
         }
 
 
     }
+
+
+
 }
+
